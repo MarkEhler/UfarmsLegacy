8db4b8622069dfb186a43e70a88c41e4e82ef457
Mark Ehler <mark@disastertech.com>
2023-08-10 12:10:24 -0400

    dependability update

--- create_users_table.sql
+++ create_users_table.sql
@@ -2,8 +2,5 @@ CREATE TABLE Users (
   UserID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   CollectorID INT,
   StartDate DATETIME,
-  Email VARCHAR(255),
-  # host 1 if they said yes to hosting land 0 if NOT
+  Email VARCHAR(255)
 );
-
-## Ufarms
--- insert_into_users.py
+++ insert_into_users.py
@@ -8,3 +8,8 @@ database_url = 'your_database_connection_string'
 engine = create_engine(database_url)
 table_name = 'Users'
 df.to_sql(table_name, con=engine, if_exists='append', index=False)
+
+csv_file = 'usr_tbl_insert.csv'
+df = pd.read_csv(csv_file)
+table_name = 'Ufarms'
+df.to_sql(table_name, con=engine, if_exists='append', index=False)
