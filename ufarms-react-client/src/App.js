673ac8877fcccee06473ec6cdd7207816dee7f98
MarkEhlerNV <mark.ehler@northviewweather.com>
2024-01-27 09:25:33 -0500

    heroku2

--- ufarms-react-client/src/App.js
+++ ufarms-react-client/src/App.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// creating an env variable for local or cloud deployments would look like
+// const apiUrl = process.env.NODE_ENV === 'prod'
+//   ? 'https://your-heroku-app-name.herokuapp.com'
+//   : 'http://localhost:8000';
+
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
+// Use apiUrl in your fetch calls
+// fetch(`${apiUrl}/time`)
   useEffect(() => {
     fetch('/time').then(res => res.json()).then(data => {
       setCurrentTime(data.time);
