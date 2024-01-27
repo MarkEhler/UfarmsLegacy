d35f1ce1908325bb151b2e070086f619e292a465
MarkEhlerNV <mark.ehler@northviewweather.com>
2024-01-27 08:42:58 -0500

    switched to fastapi for backend simplified requirements

--- run.py
+++ run.py
@@ -1,34 +1,20 @@
 import time
-from flask import Flask
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = Flask(__name__)
+app = FastAPI()
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.route('/time')
+@app.get("/time")
 def get_current_time():
     return {'time': time.time()}
 
 ### note ###
 # we will create all routes here until further notice
-
-
-# ## If things are going to be like this I might shift the stack to FastAPI newer -- it's a Flask killer
-# Fast example of the above route using Fast
-
-# from fastapi import FastAPI
-# app = FastAPI()
-# @app.get("/")
-# def get_current_time():
-#     return {'time': time.time()}
-
-
-# ---
-## A draw back of flask is that it requires these sneaky circular-prone imports -- in my experience 90% of the time this is what's causing the error, including the one today -- I think I recall a method for addressing that 
-# from app import app
-# from config import Config
-
-# app.secret_key = Config.SECRET_KEY  # Set a secret key for flash messages
-
-# # if __name__ == '__main__':
-# #     app.run(debug=True)
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000, debug=True)
