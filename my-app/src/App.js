c52d8c463f7fd4303605d3bdd4100f6c037341c2
MarkEhlerNV <mark.ehler@northviewweather.com>
2024-01-27 11:11:53 -0500

    trying this

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from 'react';
-import {Deploy} from './Component/Deploy/Deploy';
+
+// creating an env variable for local or cloud deployments would look like
+// const apiUrl = process.env.NODE_ENV === 'prod'
+//   ? 'https://your-heroku-app-name.herokuapp.com'
+//   : 'http://localhost:8000';
 
 function App() {
-  const [state, setState] = useState({})
+  const [currentTime, setCurrentTime] = useState(0);
 
+// Use apiUrl in your fetch calls
+// fetch(`${apiUrl}/time`)
   useEffect(() => {
-    fetch("/api").then(response => {
-      if(response.status == 200){
-        return response.json()
-      }
-    }).then(data => setState(data))
-    .then(error => console.log(error))
-  },[])
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <Deploy prop={state}/>
+      <header className="App-header">
+
+        ... no changes in this part ...
+
+        <p>The current time is {currentTime}.</p>
+      </header>
     </div>
   );
 }
