41a409bfb25ab26fa1e91e5ed5a701011d6a53e3
MarkEhlerNV <mark.ehler@northviewweather.com>
2024-01-29 14:57:51 -0500

    first 1.29.24

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import time
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='ufarms-react-client/build', static_url_path='/')
 
-@app.route('/time')
+@app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
 
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 ### note ###
 # we will create all routes here until further notice
 
