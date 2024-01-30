8e39d34a3972942721aed3d0d7409754b232a996
MarkEhlerNV <mark.ehler@northviewweather.com>
2024-01-29 22:55:11 -0500

    whitespace

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 import time
 from flask import Flask
 
-app = Flask(__name__)
-
+app = Flask(__name__) #, static_folder='../build', static_url_path='/')
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
