ce560fa4d620cc34581bb4e0641223591b47666c
MarkEhlerNV <mark.ehler@northviewweather.com>
2024-01-29 23:00:47 -0500

    added back front

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 import time
 from flask import Flask
 
-app = Flask(__name__) #, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='react-flask-deploy/build', static_url_path='/')
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
